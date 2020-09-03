ffa585e6994d79260b6827cccf0c0980cf665cce
Alexander <arlenross02@gmail.com>
2020-09-03 01:43:06 -0400

    Updates

---
+++ precache-manifest.6119393dd9707cb0169cdeb548c73add.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60d2d283b24b40c7f6c113a1d87ac02",
+    "url": "/c-gen/index.html"
+  },
+  {
+    "revision": "27fd3411c904752956a5",
+    "url": "/c-gen/static/css/main.8a731e9e.chunk.css"
+  },
+  {
+    "revision": "d666147bf786be566398",
+    "url": "/c-gen/static/js/2.aac10688.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/c-gen/static/js/2.aac10688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27fd3411c904752956a5",
+    "url": "/c-gen/static/js/main.2a8c8b27.chunk.js"
+  },
+  {
+    "revision": "0ac54abc31b5747514c0",
+    "url": "/c-gen/static/js/runtime-main.2f0f7266.js"
+  }
+]);
